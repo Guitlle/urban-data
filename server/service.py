3b5cca1715d7e206b6a90f9207db110f2366a4ae
Sebastian Oliva <code@sebastianoliva.com>
2014-04-12 15:25:47 -0600

    Adding server stuffz

---
+++ server/service.py
@@ -0,0 +1,20 @@
+
+
+import os
+from eve import Eve
+
+if __name__ == '__main__':
+    # Heroku support: bind to PORT if defined, otherwise default to 5000.
+    if 'PORT' in os.environ:
+        port = int(os.environ.get('PORT'))
+        # use '0.0.0.0' to ensure your REST API is reachable from all your
+        # network (and not only your computer).
+        host = '0.0.0.0'
+    else:
+        port = 5000
+        #host = '127.0.0.1'
+        host = '0.0.0.0'
+
+    app = Eve()
+    app.run(host=host, port=port)
+
