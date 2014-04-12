14247a2b5c52429a8a8231000183647b4ae98f6a
jbalsells <jorge_balsells@hotmail.com>
2014-04-12 11:58:30 +0000

    added BMP data(Temperature, Altitude,pressure)

--- client/raspclient.py
+++ client/raspclient.py
@@ -1,7 +1,35 @@
+#!/usr/bin/python
+import os
+from Adafruit_BMP085 import BMP085
 import urbandata
 
-urbandata.send( 14.123, -90.1234, {
-    'ambient_temperature': 27.2,
-    'pressure': 123.21,
-    'humidity': 123.4567
-    }) 
+os.system("sudo modprobe w1-gpio && sudo modprobe w1-therm && cd /sys/bus/w1/devices/ && ls && cd 28-000003e48b1e && cat w1_slave")
+bmp = BMP085(0x77)
+temp = bmp.readTemperature()
+pressure = bmp.readPressure()
+altitude = bmp.readAltitude()
+
+while True:
+	tfile = open("/sys/bus/w1/devices/28-000003e48b1e/w1_slave")
+	text = tfile.read()
+	tfile.close()
+	secondline = text.split("\n")[1]
+	temperaturedata = secondline.split(" ")[9]
+	temperature = float(temperaturedata[2:])
+	temperature = temperature/1000
+	print (temperature)
+	print "Temperature: %.2f C" % temp
+        print "Pressure:    %.2f hPa" % (pressure / 100.0)
+        print "Altitude:    %.2f" % altitude
+	
+        urbandata.send( 14.123, -90.1234, {
+        'temperature': temperature,
+        'barometer_BMP085_temperature1': temp,
+        'pressure': (pressure / 100.0),
+        'altitude': altitude,
+        #'humidity': 123.4567
+        }) 
+
+
+
+
