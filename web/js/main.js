4e081e27c518a021a44f0adde3442c6224a60e9a
Guillermo Ambrosio <lgar89@gmail.com>
2014-04-13 15:40:08 -0600

    demo quick stuff

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,7 @@
 function genrandata() {
   var testData = [];
-  for (var i = 0; i < 100; i++)
-    testData.push({lat: Math.random()* 0.1 + 14.6 , lon: -90.55 + Math.random()*0.1, value: Math.random() });
+  for (var i = 0; i < 200; i++)
+    testData.push({lat: Math.random()* 0.06 + 14.6 , lon: -90.55 + Math.random()*0.1, value: Math.random() });
   return testData;
 }
 
@@ -127,7 +127,7 @@ $(function() {
       }
   });
 
-//   heatmapLayer.addData(genrandata());
+   heatmapLayer.addData(genrandata());
   humidityLayer.addData(genrandata());
   noiseLayer.addData(genrandata());
   co2Layer.addData(genrandata());
@@ -145,36 +145,36 @@ $(function() {
   var controls = L.control.layers(null, overlayMaps, {collapsed: false});
 
   var map = new L.Map('map', {
-      center: new L.LatLng(14.634, -90.5),
+      center: new L.LatLng(14.604698, -90.489502),
       zoom: 13,
       layers: [baseLayer, heatmapLayer],
       scrollWheelZoom: false
   });
   
-  $.ajax({url: 'http://198.199.98.147:5000/data_point', 
-	 success: function(data) 
-	 { 
-	   var tempData = [];
-	   var myLayer = L.geoJson().addTo(map);
-	   
-	   $.each(data._items, function (i,e) 
-	   {
-	     if (e.properties.temperature) 
-	     {
-	      tempData.push({ lat: e.geometry.coordinates[0], lon: e.geometry.coordinates[1], value: e.properties.temperature});
-	      var temp = e.geometry.coordinates[0];
-	      e.geometry.coordinates[0] = e.geometry.coordinates[1];
-	      e.geometry.coordinates[1] = temp;
-	      console.log(e);
-	      myLayer.addData(e);
-	     }
-	   });
-	   myLayer.addTo(map);
-	   console.log(map);
-	   //console.log(tempData);
-	   heatmapLayer.addData(tempData);
-	   heatmapLayer.redraw();
-	} });
+//   $.ajax({url: 'http://198.199.98.147:5000/data_point', 
+// 	 success: function(data) 
+// 	 { 
+// 	   var tempData = [];
+// 	   var myLayer = L.geoJson().addTo(map);
+// 	   
+// 	   $.each(data._items, function (i,e) 
+// 	   {
+// 	     if (e.properties.temperature) 
+// 	     {
+// 	      tempData.push({ lat: e.geometry.coordinates[0], lon: e.geometry.coordinates[1], value: e.properties.temperature});
+// 	      var temp = e.geometry.coordinates[0];
+// 	      e.geometry.coordinates[0] = e.geometry.coordinates[1];
+// 	      e.geometry.coordinates[1] = temp;
+// 	      console.log(e);
+// 	      myLayer.addData(e);
+// 	     }
+// 	   });
+// 	   myLayer.addTo(map);
+// 	   console.log(map);
+// 	   //console.log(tempData);
+// 	   heatmapLayer.addData(tempData);
+// 	   heatmapLayer.redraw();
+// 	} });
   
   controls.addTo(map);
 
