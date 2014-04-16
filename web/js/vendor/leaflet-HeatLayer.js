fe9bd2f908b19ad92a119f6f954e1c75fa7c9c61
Guillermo Ambrosio <lgar89@gmail.com>
2014-04-16 01:06:52 -0600

    working locally relative heat map

--- web/js/vendor/leaflet-HeatLayer.js
+++ web/js/vendor/leaflet-HeatLayer.js
@@ -134,15 +134,35 @@ L.HeatLayer = L.Class.extend({
             offsetY = panePos.y % cellSize,
             i, len, p, cell, x, y, j, len2, k;
 
-        // console.time('process');
+        var max=false, min=false;
+        for (i = 0, len = this._latlngs.length; i < len; i++) {
+            if (bounds.contains(this._latlngs[i])) {
+                if (min == false)
+                    min = this._latlngs[i][2];
+                if (min < this._latlngs[i][2])
+                    min = this._latlngs[i][2];
+                if (max == false)
+                    max = this._latlngs[i][2];
+                if (max > this._latlngs[i][2])
+                    max = this._latlngs[i][2];
+            }
+        }
+        var range = max-min, offset = min;
+        // unlikely situation
+        if (range == 0) {
+            plain = true;
+        }
+
         for (i = 0, len = this._latlngs.length; i < len; i++) {
             if (bounds.contains(this._latlngs[i])) {
                 p = this._map.latLngToContainerPoint(this._latlngs[i]);
                 x = Math.floor((p.x - offsetX) / cellSize) + 2;
                 y = Math.floor((p.y - offsetY) / cellSize) + 2;
-
-                k = (this._latlngs[i].alt || 1) * v;
-
+                if (plain)
+                    k = 0.5;
+                else
+                    k = (this._latlngs[i][2]-offset)/range;
+                // debugger;
                 grid[y] = grid[y] || [];
                 cell = grid[y][x];
 
